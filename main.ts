fbd3565223e4c1739092651ffdb93535166a0c37
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-04 11:03:51 -0400

    Comments added

--- main.ts
+++ main.ts
@@ -11,20 +11,26 @@ pins.digitalWritePin(DigitalPin.P14, 0)
 pins.digitalWritePin(DigitalPin.P13, 0)
 basic.showIcon(IconNames.Happy)
 
+//Begins by turning the LED Red
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString("Blue")
     pins.digitalWritePin(DigitalPin.P15, 0)
 
+//Turns LED Green
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString("Green")
     basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P14, 0)
 
+//Turns LED red
+
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Red")
     pins.digitalWritePin(DigitalPin.P13, 0)
     basic.clearScreen()
+
+//Turns LED Cyan
 
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
@@ -33,17 +39,26 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 0)
     basic.clearScreen()
 
+//Turns LED Yellow
+
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Yellow")
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
     basic.clearScreen()
-    
+
+//Turns LED Magenta
+
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Magenta")
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
     basic.clearScreen()
+
+//Ends of by showing a happy face
+
+    basic.showIcon(IconNames.Happy)
+
 })
