3ed5f1efadc61186401b8117c86d43f2829079a2
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-04 10:55:50 -0400

    continuous code editing

--- main.ts
+++ main.ts
@@ -13,24 +13,31 @@ basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P15, 1)
-    basic.showString("Red")
+    basic.showString("Blue")
     pins.digitalWritePin(DigitalPin.P15, 0)
 
     pins.digitalWritePin(DigitalPin.P14, 1)
-    basic.showString("Blue")
+    basic.showString("Green")
     basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P14, 0)
 
     pins.digitalWritePin(DigitalPin.P13, 1)
-    basic.showString("Green")
-    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Red")
+    pins.digitalWritePin(DigitalPin.P13, 0)
     basic.clearScreen()
 
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
-    basic.showString("Red")
+    basic.showString("Cyan")
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     basic.clearScreen()
 
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Cyan")
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.clearScreen()
+    
 })
